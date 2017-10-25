7f8f777628c66d26b4a5e6e256a090b46f7ca134
william ferrufino <william.ferrufino@solarwinds.com>
2017-10-25 14:33:51 -0500

    cleanup

--- js/main.js
+++ js/main.js
@@ -13,12 +13,14 @@ app.config(function($routeProvider) {
 
 (function(app){
   "use strict";
-  app.controller("ShopCtrl", function($scope){
+
+app.controller("ShopCtrl", function($scope){
    	$scope.items = [
-		{id: 1, text: 'Item 1', bought: true},
-		{id: 2, text: 'Item 2', bought: false},
-		{id: 3, text: 'Item 3', bought: false}
-	];
+  		{id: 1, text: 'Item 1', bought: true},
+  		{id: 2, text: 'Item 2', bought: false},
+  		{id: 3, text: 'Item 3', bought: false}
+  	];
+
 	$scope.addItem = function() {
 		$scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
 		$scope.itemEntry = '';
