d10eef26ec9b99fbd5d14b45135a2ef852c74163
william ferrufino <william.ferrufino@solarwinds.com>
2017-10-25 12:38:24 -0500

    input form controller

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var app = angular.module("app", ["ngRoute"]);
+
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
@@ -7,8 +8,26 @@ app.config(function($routeProvider) {
     .when("/list", {
         templateUrl : "templates/list.html"
     })
-
-
-
-
 });
+
+
+(function(app){
+  "use strict";
+  app.controller("ShopCtrl", function($scope){
+   	$scope.items = [
+		{id: 1, text: 'Milk', bought: true},
+		{id: 2, text: 'Bread', bought: false},
+		{id: 3, text: 'Cheese', bought: false}
+	];
+	$scope.addItem = function() {
+		$scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
+		$scope.itemEntry = '';
+	}
+	
+  });
+})(app);
+
+
+
+
+
