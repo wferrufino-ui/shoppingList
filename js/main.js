cb6533da909e543599ec3bc0d1cb78549ec9abb8
wferrufino <william.ferrufino@solarwinds.com>
2017-10-25 19:33:08 -0500

    list items form updates

--- js/main.js
+++ js/main.js
@@ -21,16 +21,19 @@ app.controller("ShopCtrl", function($scope){
   		{id: 3, text: 'Item 3', bought: false} */
   	];
 
-	$scope.addItem = function() {
-		$scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
-		$scope.itemEntry = '';
-	}
+    $scope.clearBought = function() {
+      $scope.items = _.filter($scope.items, function(item) {
+        return !item.bought;
+      });
+    }
+
+  	$scope.addItem = function() {
+  		$scope.items.push({text: $scope.itemEntry, bought: false, id: ($scope.items.length + 1) });
+  		$scope.itemEntry = '';
+  	}
 	
 
   });
 })(app);
-
-
-
 
 
